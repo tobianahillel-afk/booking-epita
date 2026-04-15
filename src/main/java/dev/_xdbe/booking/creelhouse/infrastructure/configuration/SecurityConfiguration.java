bac91c3cdd056ecd54d5bf9d3642dea65ff098f0
hillel tobiana <tobianahillel-afk@users.noreply.github.com>
2026-04-15 10:24:28 +0200

    feat: add login form

--- src/main/java/dev/_xdbe/booking/creelhouse/infrastructure/configuration/SecurityConfiguration.java
+++ src/main/java/dev/_xdbe/booking/creelhouse/infrastructure/configuration/SecurityConfiguration.java
@@ -1,21 +1,15 @@
 package dev._xdbe.booking.creelhouse.infrastructure.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.session.web.http.CookieSerializer;
-import org.springframework.session.web.http.DefaultCookieSerializer;
+
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
@@ -26,14 +20,10 @@ public class SecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
             .authorizeHttpRequests(auth -> auth
-                // Step 4a: add access control
-                // ...
-                // Step 4a: end
+                .requestMatchers("/dashboard").hasRole("ADMIN")
                 .anyRequest().permitAll()
             )
-            // Step 4b: Add login form
-            // ...
-            // Step 4b: End of login form configuration
+            .formLogin(withDefaults())
             
             .csrf((csrf) -> csrf
                 .ignoringRequestMatchers("/h2-console/**")
@@ -47,7 +37,22 @@ public class SecurityConfiguration {
     }
 
     // Step 3: add InMemoryUserDetailsManager
-    // ...
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails administrator = User.builder()
+            .username("admin")
+            .password("{bcrypt}$2b$10$sRxMf0HwPCXBWLrKuCt.XeKyQ0nJqkOccuuR90B9lx3iykO3k28em")
+            .roles("ADMIN")
+            .build();
+
+        UserDetails guest = User.builder()
+            .username("guest")
+            .password("{bcrypt}$2b$10$v3w4F4vEid3VsLVP1kLB4eKwiCsIRl/zUPijaNg4ycWGGuMxpta66")
+            .roles("GUEST")
+            .build();
+
+        return new InMemoryUserDetailsManager(administrator, guest);
+    }
     // Step 3: end
 
 }
